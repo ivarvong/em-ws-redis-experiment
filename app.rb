fe9a86a42297114deb8d853bcd73d1f3960c18a4
Ivar Vong <ivar@ivarvong.com>
2013-05-15 21:36:31 -0700

    wow, bad editor settings. nuke tabs

--- app.rb
+++ app.rb
@@ -8,75 +8,75 @@ require 'em-hiredis'
 require 'thin'
 
 class App < Sinatra::Base
-	use Rack::FiberPool
-	use Rack::CommonLogger
+    use Rack::FiberPool
+    use Rack::CommonLogger
 
-	def initialize
-    	super
-		@sockets = []    	    	
+    def initialize
+        super
+        @sockets = []               
 
-		@redis = EM::Hiredis.connect #used for LPUSH and LLEN
-		@redis_block = EM::Hiredis.connect #used with BRPOP to watch the queue list
-		@pubsub = @redis.pubsub
+        @redis = EM::Hiredis.connect #used for LPUSH and LLEN
+        @redis_block = EM::Hiredis.connect #used with BRPOP to watch the queue list
+        @pubsub = @redis.pubsub
 
-    	EventMachine.next_tick{ pop_message() } #set up BRPOP the first time
+        EventMachine.next_tick{ pop_message() } #set up BRPOP the first time
 
-    	@redis.pubsub.psubscribe("*") { |channel, message|     		
-    		if channel == "broadcast"
-	  			EventMachine.next_tick {
-	  				@sockets.each{|s| s.send "#{message}"}
-	  			}  			
-  			end	
-		}
+        @redis.pubsub.psubscribe("*") { |channel, message|          
+            if channel == "broadcast"
+                EventMachine.next_tick {
+                    @sockets.each{|s| s.send "#{message}"}
+                }           
+            end 
+        }
 
-		timer = EventMachine::PeriodicTimer.new(2) do
-  			@redis.llen("incoming_messages"){|res|
-  				puts "LLEN: #{res}"
-  			}
-		end
-  	end	
+        timer = EventMachine::PeriodicTimer.new(2) do
+            @redis.llen("incoming_messages"){|res|
+                puts "LLEN: #{res}"
+            }
+        end
+    end 
 
-	get '/' do
-		erb :index
-	end
+    get '/' do
+        erb :index
+    end
 
-	get '/ws' do
-		request.websocket do |ws|
-			ws.onopen do
-				@sockets << ws
-				update_client_count()
-			end
-			ws.onclose do
-				@sockets.delete ws
-				update_client_count()
-			end
-			ws.onmessage do |msg| 											
-				@redis.lpush('incoming_messages', msg).callback{|res|
-					#all is well?
-				}.errback{|res|
-					puts "ERROR! LPUSH-IN'" #? not tested.
-				}				
-			end
-    	end
-	end
+    get '/ws' do
+        request.websocket do |ws|
+            ws.onopen do
+                @sockets << ws
+                update_client_count()
+            end
+            ws.onclose do
+                @sockets.delete ws
+                update_client_count()
+            end
+            ws.onmessage do |msg|                                           
+                @redis.lpush('incoming_messages', msg).callback{|res|
+                    #all is well?
+                }.errback{|res|
+                    puts "ERROR! LPUSH-IN'" #? not tested.
+                }               
+            end
+        end
+    end
 
-	def update_client_count
-		broadcast({id: 'count', data: @sockets.count})
-	end
+    def update_client_count
+        broadcast({id: 'count', data: @sockets.count})
+    end
 
-  	def pop_message
-  		@redis_block.brpop('incoming_messages', 0).callback{|key, message|
-  			puts "BRPOP grabbed: #{message} (on '#{key}')"	  			
-  			broadcast({id:'message', data:message})
-  			#EventMachine.add_timer(0.10) { # don't push to the client more than 10Hz
-  			EventMachine.next_tick{
-  				pop_message()
-  			}	
-  		}	  				
-  	end
-	
-	def broadcast(message) 
-		@redis.publish 'broadcast', "#{message.to_json}"
-	end
-	
+    def pop_message
+        @redis_block.brpop('incoming_messages', 0).callback{|key, message|
+            puts "BRPOP grabbed: #{message} (on '#{key}')"              
+            broadcast({id:'message', data:message})
+            #EventMachine.add_timer(0.10) { # don't push to the client more than 10Hz
+            EventMachine.next_tick{
+                pop_message()
+            }   
+        }                   
+    end
+    
+    def broadcast(message) 
+        @redis.publish 'broadcast', "#{message.to_json}"
+    end
+    
 end
